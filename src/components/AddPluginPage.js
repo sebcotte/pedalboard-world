a08ac8aa2fba93ac9f753c225017c4b3fcaf3475
Cotte Sebatien <sebastien.cotte@etu.unice.fr>
2018-11-27 18:28:37 +0100

    Add a parameter in the form (v1.1)

--- src/components/AddPluginPage.js
+++ src/components/AddPluginPage.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import './AddPluginPage.css';
-import { Form, Select, Upload, Button, Icon, Input } from 'antd';
+import { Form, Select, Upload, Button, Icon, Input, Table, Popconfirm } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
-
-const FormItem = Form.Item;
-const Option = Select.Option;
 
 // un vendeur : URL du site du créateur
 // nom du créateur (kuiza)
@@ -14,9 +11,234 @@ const Option = Select.Option;
 // descritption
 // un table de détails : control, default, min, max, 
 
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+    state = {
+      editing: false,
+    }
+  
+    componentDidMount() {
+      if (this.props.editable) {
+        document.addEventListener('click', this.handleClickOutside, true);
+      }
+    }
+  
+    componentWillUnmount() {
+      if (this.props.editable) {
+        document.removeEventListener('click', this.handleClickOutside, true);
+      }
+    }
+  
+    toggleEdit = () => {
+      const editing = !this.state.editing;
+      this.setState({ editing }, () => {
+        if (editing) {
+          this.input.focus();
+        }
+      });
+    }
+  
+    handleClickOutside = (e) => {
+      const { editing } = this.state;
+      if (editing && this.cell !== e.target && !this.cell.contains(e.target)) {
+        this.save();
+      }
+    }
+  
+    save = () => {
+      const { record, handleSave } = this.props;
+      this.form.validateFields((error, values) => {
+        if (error) {
+          return;
+        }
+        this.toggleEdit();
+        handleSave({ ...record, ...values });
+      });
+    }
+  
+    render() {
+      const { editing } = this.state;
+      const {
+        editable,
+        dataIndex,
+        title,
+        record,
+        index,
+        handleSave,
+        ...restProps
+      } = this.props;
+      return (
+        <td ref={node => (this.cell = node)} {...restProps}>
+          {editable ? (
+            <EditableContext.Consumer>
+              {(form) => {
+                this.form = form;
+                return (
+                  editing ? (
+                    <FormItem style={{ margin: 0 }}>
+                      {form.getFieldDecorator(dataIndex, {
+                        rules: [{
+                          required: true,
+                          message: 'Ce champs est requis',
+                        }],
+                        initialValue: record[dataIndex],
+                      })(
+                        <Input
+                          ref={node => (this.input = node)}
+                          onPressEnter={this.save}
+                        />
+                      )}
+                    </FormItem>
+                  ) : (
+                    <div
+                      className="editable-cell-value-wrap"
+                      style={{ paddingRight: 24 }}
+                      onClick={this.toggleEdit}
+                    >
+                      {restProps.children}
+                    </div>
+                  )
+                );
+              }}
+            </EditableContext.Consumer>
+          ) : restProps.children}
+        </td>
+      );
+    }
+}
+
+class EditableTable extends React.Component {
+    constructor(props) {
+      super(props);
+      this.columns = [{
+        title: 'Contrôle',
+        dataIndex: 'control',
+        width: '30%',
+        editable: true,
+      }, {
+        title: 'Par défaut',
+        dataIndex: 'default',
+        width: '15%',
+        editable: true,
+      }, {
+        title: 'Min',
+        dataIndex: 'min',
+        width: '15%',
+        editable: true,
+      }, 
+      {
+        title: 'Max',
+        dataIndex: 'max',
+        width: '15%',
+        editable: true,
+      }, {
+        title: 'Action',
+        dataIndex: 'operation',
+        render: (text, record) => {
+          return (
+            this.state.dataSource.length >= 1
+              ? (
+                <Popconfirm title="Vous êtes sûr ?" onConfirm={() => this.handleDelete(record.key)}>
+                  <a href="javascript:;">Supprimer</a>
+                </Popconfirm>
+              ) : null
+          );
+        },
+      }];
+  
+      this.state = {
+        dataSource: [], 
+        count: 0,
+        displayTable: false
+      };
+    }
+  
+    handleDelete = (key) => {
+      const dataSource = [...this.state.dataSource];
+      this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
+    }
+  
+    handleAdd = () => {
+      const { count, dataSource } = this.state;
+      const newData = {
+        key: count,
+        control: 'Contrôle',
+        default: 0,
+        min: 0,
+        max: 0
+      };
+      this.setState({
+        dataSource: [...dataSource, newData],
+        count: count + 1,
+        displayTable: true
+      });
+    }
+  
+    handleSave = (row) => {
+      const newData = [...this.state.dataSource];
+      const index = newData.findIndex(item => row.key === item.key);
+      const item = newData[index];
+      newData.splice(index, 1, {
+        ...item,
+        ...row,
+      });
+      this.setState({ dataSource: newData });
+    }
+  
+    render() {
+      const { dataSource } = this.state;
+      const components = {
+        body: {
+          row: EditableFormRow,
+          cell: EditableCell,
+        },
+      };
+      const columns = this.columns.map((col) => {
+        if (!col.editable) {
+          return col;
+        }
+        return {
+          ...col,
+          onCell: record => ({
+            record,
+            editable: col.editable,
+            dataIndex: col.dataIndex,
+            title: col.title,
+            handleSave: this.handleSave,
+          }),
+        };
+      });
+      return (
+        <div>
+          <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
+            Ajouter un paramètre
+          </Button>
+          <Table
+            components={components}
+            rowClassName={() => 'editable-row'}
+            bordered
+            dataSource={dataSource}
+            columns={columns}
+          />
+        </div>
+      );
+    }
+}
 
 class AddPluginPage extends React.Component {
-
+    
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
@@ -125,13 +347,14 @@ class AddPluginPage extends React.Component {
                     )}
                 </FormItem>
 
+                <EditableTable />   
+
                 <FormItem
                     wrapperCol={{ span: 12, offset: 6 }}
                 >
                     <Button type="primary" htmlType="submit">Valider</Button>
                     <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>Clear</Button>
-                </FormItem>              
-
+                </FormItem>          
             </Form>
         );
     } 
